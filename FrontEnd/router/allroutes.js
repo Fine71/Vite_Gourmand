987832da78c56783626ef64951f743145ee4153d
Fine71 <delphine.esparon@gmail.com>
2026-04-06 16:34:05 +0200

    création routes

---
+++ FrontEnd/router/allroutes.js
@@ -0,0 +1,11 @@
+import Route from "./route.js";
+
+//Définition des routes
+export const allRoutes = [
+new Route("/", "Accueil", "/pages/home.html"),
+
+]
+
+// Le titre s'affiche comme ceci : Route.titre - websiteName
+
+export const websiteName = "Vite & Gourmand";
