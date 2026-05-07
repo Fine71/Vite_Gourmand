4ccb3b0694a239dd3c22c4a3732af1b4e8b400c1
Delphine Esparon <delphine.esparon@gmail.com>
2026-05-07 12:43:39 +0200

    création signin.js et signup.js

--- FrontEnd/router/allRoutes.js
+++ FrontEnd/router/allRoutes.js
@@ -6,8 +6,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/menus", "Menus", "/pages/menus.html"),
     new Route("/details", "Détails", "/pages/menus/details.html", [], "/pages/js/global.js"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", []),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", [])
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/pages/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/pages/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["connected"]),
+    new Route("/editPassword", "Modifier le mot de passe", "/pages/auth/editPassword.html", []),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
 
 ]
 
