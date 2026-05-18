13f7b6ce07e42a6403207d1c0b193a7ae2707151
Delphine Esparon <delphine.esparon@gmail.com>
2026-05-18 21:22:57 +0200

    installation symfony + creation DBD

--- FrontEnd/router/allRoutes.js
+++ FrontEnd/router/allRoutes.js
@@ -5,7 +5,6 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/menus", "Menus", "/pages/menus.html", []),
-    new Route("/details", "Détails", "/pages/menus/details.html", [], "/pages/js/global.js"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/pages/js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/pages/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin", "employé"]),
